65c0006cdefa0392350c402d94b2a2ce9476a527
Raza Rauf <m.razarauf@gmail.com>
2019-06-17 23:06:01 -0400

    create initial model classes and migrations

---
+++ Vidly/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        // Navigation property - allows us to navigate from one type to another
+        // useful when we want to load an object and its related objects from the db
+        public MembershipType Membership { get; set; }
+        // foreign key - not loading the whole object as above
+        public byte MembershipTypeId { get; set; }
+    }
+}
