20989d1c4c2beee75ea4fb539fb8e1f84d267e85
Raza Rauf <m.razarauf@gmail.com>
2019-06-19 22:17:50 -0400

    Apply Annotations To Customer Name

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,18 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
+        // data annotations:
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
 
         // Navigation property - allows us to navigate from one type to another
         // useful when we want to load an object and its related objects from the db
         public MembershipType Membership { get; set; }
+
         // foreign key - not loading the whole object as above
         public byte MembershipTypeId { get; set; }
     }
