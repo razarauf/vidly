a604c0b03216cf4399942433e5e308b31a70503e
Raza Rauf <m.razarauf@gmail.com>
2019-06-21 21:32:43 -0400

    remove customer ctor

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -23,10 +23,5 @@ namespace Vidly.Models
 
         // foreign key - not loading the whole object as above
         public byte MembershipTypeId { get; set; }
-
-        public Customer()
-        {
-
-        }
     }
 }
