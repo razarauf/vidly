adb03d701b56258e392fa743a1ea85b24bbe736d
Raza Rauf <m.razarauf@gmail.com>
2019-06-24 21:15:21 -0400

    Exercise 3 - Add Movie and Genre model

---
+++ Vidly/Migrations/201906250112310_AddMovieAndGenreModel.cs
@@ -0,0 +1,45 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMovieAndGenreModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 255),
+                        ReleaseDate = c.DateTime(nullable: false),
+                        DateAdded = c.DateTime(nullable: false),
+                        NumberInStock = c.Int(nullable: false),
+                        GenreID = c.Byte(nullable: false),
+                        Genre_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Genres", t => t.Genre_Id, cascadeDelete: true)
+                .Index(t => t.Genre_Id);
+            
+            CreateTable(
+                "dbo.Genres",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Movies", "Genre_Id", "dbo.Genres");
+            DropIndex("dbo.Movies", new[] { "Genre_Id" });
+            DropTable("dbo.Genres");
+            DropTable("dbo.Movies");
+        }
+    }
+}
---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public int NumberInStock { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+        public byte GenreID { get; set; }
+    }
+}
